60a625e91f353bd443c94f36c5dff14f31d3983a
Tommaso Teofili <teofili@adobe.com>
2012-09-04 09:38:16 +0200

    adding uima module with NameAnnotator

---
+++ uima/src/main/java/com/github/tteofili/btl/uima/annotator/NameAnnotator.java
@@ -0,0 +1,61 @@
+package com.github.tteofili.btl.uima.annotator;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_component.CasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.TypeSystem;
+import org.apache.uima.cas.text.AnnotationFS;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.util.Level;
+
+/**
+ * Annotates first names using a name dictionary and PoSs
+ */
+public class NameAnnotator extends CasAnnotator_ImplBase {
+
+    private static final String NAMESDICT_PATH = "dictpath";
+    private static final String POS_FEATURENAME = "posTag";
+
+    private List<String> namesList;
+
+    @Override
+    public void initialize(UimaContext aContext) throws ResourceInitializationException {
+        super.initialize(aContext);
+        try {
+            namesList = IOUtils.readLines(new FileInputStream(String.valueOf(aContext
+                    .getConfigParameterValue(NAMESDICT_PATH))));
+            Collections.sort(namesList); // sort collection to make binary search possible
+        } catch (IOException e) {
+            aContext.getLogger().log(Level.SEVERE, "error reading dictionary file");
+            throw new ResourceInitializationException(e);
+        }
+    }
+
+    @Override
+    public void process(CAS cas) throws AnalysisEngineProcessException {
+        TypeSystem typeSystem = cas.getTypeSystem();
+        for (AnnotationFS annotation : cas.getAnnotationIndex(typeSystem.getType("org.apache.uima.TokenAnnotation"))) {
+            /* create a name annotation for each occurrence of a name in the dictionary */
+            if (Collections.binarySearch(namesList, annotation.getCoveredText()) >= 0 && isNP(annotation)) {
+                AnnotationFS nameAnnotation = cas.createAnnotation(typeSystem.
+                        getType("com.github.tteofili.btl.uima.NameAnnotation"), annotation.getBegin(), annotation.getEnd());
+                cas.addFsToIndexes(nameAnnotation);
+            }
+        }
+    }
+
+    private boolean isNP(AnnotationFS annotation) {
+        Type type = annotation.getType();
+        String pos = annotation.getFeatureValueAsString(type.getFeatureByBaseName(POS_FEATURENAME));
+        return pos != null && "np".equalsIgnoreCase(pos);
+    }
+
+}
