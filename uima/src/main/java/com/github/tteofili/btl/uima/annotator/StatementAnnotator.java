d76c45257c0bed6d07284fef757a93f12a88640f
Tommaso Teofili <teofili@adobe.com>
2013-11-27 12:27:47 +0100

    first dummy impl of statement annotator

--- uima/src/main/java/com/github/tteofili/btl/uima/annotator/StatementAnnotator.java
+++ uima/src/main/java/com/github/tteofili/btl/uima/annotator/StatementAnnotator.java
@@ -1,6 +1,5 @@
 package com.github.tteofili.btl.uima.annotator;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.uima.analysis_component.CasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
@@ -14,24 +13,40 @@ import org.apache.uima.util.Level;
  */
 public class StatementAnnotator extends CasAnnotator_ImplBase {
 
-    private final static Pattern p = Pattern.compile(".*\\\"(.*)\\\".*");
+    private final static Pattern p = Pattern.compile(".*\\\"(\\w+\\s*)\\\".*");
+
 
     @Override
     public void process(CAS cas) throws AnalysisEngineProcessException {
         // for each sentence
         TypeSystem typeSystem = cas.getTypeSystem();
         for (AnnotationFS annotation : cas.getAnnotationIndex(typeSystem.getType(AnnotationUtils.SENTENCE_ANNOTATION))) {
-            /* create a name annotation for each occurrence of a name in the dictionary */
-//            if (Collections.binarySearch(namesList, annotation.getCoveredText()) >= 0 && isNP(annotation)) {
-//                AnnotationFS nameAnnotation = cas.createAnnotation(typeSystem.
-//                        getType(AnnotationUtils.NAME_ANNOTATION), annotation.getBegin(), annotation.getEnd());
-//                cas.addFsToIndexes(nameAnnotation);
-//            }
+
+            String sentence = annotation.getCoveredText();
+            this.getContext().getLogger().log(Level.INFO, "working on sentence: " + sentence);
+
             // find all the text fragments between apexes
             try {
-                Matcher m = p.matcher(annotation.getCoveredText());
-                while (m.find()) {
-                    cas.createAnnotation(typeSystem.getType(AnnotationUtils.STATEMENT_ANNOTATION), m.start(1), m.end(1));
+                int start = 0;
+                while ((start = sentence.indexOf("\"", start)) >= 0) {
+
+                    // get rid of "/ tokens
+                    while (sentence.charAt(start + 1) == '/') {
+                        start = sentence.indexOf("\"", start + 1);
+                    }
+                    int end = sentence.indexOf("\"", start + 1);
+                    while (sentence.charAt(end + 1) == '/') {
+                        end = sentence.indexOf("\"", end + 1);
+                    }
+
+                    if (start > -1 && end > start) {
+                        AnnotationFS statementAnnotation = cas.createAnnotation(typeSystem.getType(AnnotationUtils.STATEMENT_ANNOTATION), annotation.getBegin() + start + 1, annotation.getBegin() + end);
+                        cas.addFsToIndexes(statementAnnotation);
+                        this.getContext().getLogger().log(Level.INFO, "statement found: " + statementAnnotation.getCoveredText());
+                        start = end + 1;
+                    } else {
+                        break;
+                    }
                 }
             } catch (Exception e) {
                 this.getContext().getLogger().log(Level.WARNING, "cannot create StatementAnnotation", e);
