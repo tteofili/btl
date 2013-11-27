d0b9c481daf012db071a5f341e868a6dbce2ca8b
Tommaso Teofili <teofili@adobe.com>
2013-11-27 16:22:40 +0100

    sdc avoids crawling same child multiple times

--- crawler/src/main/java/com/github/tteofili/btl/crawler/Crawler.java
+++ crawler/src/main/java/com/github/tteofili/btl/crawler/Crawler.java
@@ -8,9 +8,23 @@ import java.util.Collection;
  */
 public interface Crawler {
 
+    /**
+     * get a {@link Page} given its {@link URL}
+     *
+     * @param url the page URL
+     * @return a {@link Page} located at the given {@link URL}
+     * @throws CrawlingException
+     */
     public Page getPage(URL url) throws CrawlingException;
 
     public Site getSite(URL url) throws CrawlingException;
 
+    /**
+     * get all the {@link Page}s linked by the given root {@link Page}
+     *
+     * @param page the starting page to get linked {@link Page}s from
+     * @return a {@link Collection} of linked {@link Page}s
+     * @throws CrawlingException
+     */
     public Collection<Page> getLinkedPages(Page page) throws CrawlingException;
 }
